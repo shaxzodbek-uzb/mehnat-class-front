5b56c740448b5d14f4277893f4932fe1b2625a6c
farrukh <choriyevfarrux997@gmail.com>
2020-09-09 17:14:23 +0500

    change

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,56 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <CNavbar expandable="md" type="dark" color="info">
+        <CToggler in-navbar @click="collapsed = !collapsed"/>
+        <CNavbarBrand href="#">NavBar</CNavbarBrand>
+        <CCollapse :show="collapsed" navbar>
+          <CNavbarNav>
+       |
+            <CNavItem href=""><router-link to="/">Home</router-link></CNavItem>
+            <CNavItem href=""><router-link to="/about">About</router-link></CNavItem>
+          </CNavbarNav>
+
+          <!-- Right aligned nav items -->
+          <CNavbarNav class="ml-auto">
+            <CForm inline>
+              <CInput
+                class="mr-sm-2"
+                placeholder="Search"
+                size="sm"
+              />
+              <CButton
+                color="secondary"
+                size="sm" 
+                class="my-2 my-sm-0" 
+                type="submit"
+              >
+                Search
+              </CButton>
+            </CForm>
+
+            <CDropdown
+              togglerText="Lang"
+              nav
+              placement="bottom-end"
+            >
+                <CDropdownItem>EN</CDropdownItem>
+                <CDropdownItem>ES</CDropdownItem>
+                <CDropdownItem>RU</CDropdownItem>
+                <CDropdownItem>FA</CDropdownItem>
+            </CDropdown>
+
+            <CDropdown
+              nav
+              togglerText="User"
+              placement="bottom-end"
+            >
+              <CDropdownItem>Profile</CDropdownItem>
+              <CDropdownItem>Signout</CDropdownItem>
+            </CDropdown>
+          </CNavbarNav>
+        </CCollapse>
+      </CNavbar>
     </div>
     <router-view />
   </div>
