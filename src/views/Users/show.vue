167bb3861fedb1d594a6d11cd812afaa0731c74f
Xudoynazarov Xushnudbek <xushnud.khan@gmail.com>
2020-10-02 19:06:18 +0500

    user profile show

--- src/views/Users/show.vue
+++ src/views/Users/show.vue
@@ -4,12 +4,52 @@
      <CIcon name="cilArrowLeft" /> Foydalanuvchilar ro'yhatiga qaytish
     </router-link>
     <CCard>
-        <CCardHeader>
-            Profile
-        </CCardHeader>
-        <CCardBody>
-          {{ item }}
-        </CCardBody>
+      <CCardHeader>
+          Profile
+      </CCardHeader>
+      <CCardBody>
+        <CRow>
+          <CCol col="2">
+            <CRow>
+              <img src="@/assets/images/avatar.png" width="130" alt />
+            </CRow>
+            <p><strong>Fullname: </strong> {{ item.fullname }}</p>
+            <p><strong>Age: </strong> {{ item.age }} y.o.</p>
+          </CCol>
+
+          <CCol col="5">
+            <CRow v-for="(article, index) in item.articles.data" :key="index">
+
+              <CCallout color="info">
+                <small class="text-muted">{{ article.alias }}</small><br>
+                <strong class="h5" @click="showComments = !showComments" style="cursor: pointer;">{{ article.text }}</strong>
+              </CCallout>
+
+              <CCollapse :show="showComments" class="mt-2">
+
+                <template v-if="article.comments && article.comments.length != 0 ">
+                  <CCol col="12" sm="12">
+                    <CWidgetIcon
+                      v-for="comment in article.comments.data"
+                      :key="comment.id"
+                      :header="comment.user ? comment.user.username : 'Unknown'"
+                      :text="comment.text"
+                    >
+                      <img src="@/assets/images/avatar_w.png" width="50" alt>
+                    </CWidgetIcon>
+                    
+                  </CCol>
+                </template>
+
+                <template v-else>
+                  <CCard body-wrapper class="text-muted"> Izoh mavjud emas </CCard>
+                </template>
+              </CCollapse>
+            </CRow>
+          </CCol>
+
+        </CRow>
+      </CCardBody>
     </CCard>
   </CContainer>
 </template>
@@ -25,12 +65,13 @@ export default {
       item: [],
       fields: usersData.fields,
       details: [],
-      collapseDuration: 0
+      collapseDuration: 0,
+      showComments: false
     };
   },
   mounted() {
         let id = this.$route.params.id;
-        this.$api(`users/${id}`, { params: {include: 'articles.comments'}}).then(({data}) => {
+        this.$api(`users/${id}`, { params: {include: 'articles.comments'}}).then(({ data: {data}}) => {
             this.item = data
     });
   },
